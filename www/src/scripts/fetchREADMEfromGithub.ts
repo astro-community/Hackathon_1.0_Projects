5a593f2ae4adceef94e5bd301d5f1c3eca131b26
delucis <swithinbank@gmail.com>
2022-05-21 22:40:14 +0200

    Fix READMEs fetched from GitHub
    The previous technique for fetching GitHub READMEs was a bit broken because it relied on the repo’s default branch being `main`, which turned out not to always be the case.
    
    This commit uses the GitHub REST API to fetch the READMEs instead. This requires a GitHub Access Token to be added to the build environment variables as `PUBLIC_GITHUB_TOKEN`, but is a much more reliable way to get the content.
    
    Once I’d done that, I realised a bunch more rendering limitations with just plonking the raw Markdown from GitHub on a page: broken images & links and rendering anomalies. So I’ve built a remark pipeline to try to quickly emulate GitHub’s rendering and fix the images and links. It’s a bit like npm’s `marky-markdown`, but that doesn’t seem to be compatible with modern projects.

--- www/src/scripts/fetchREADMEfromGithub.ts
+++ www/src/scripts/fetchREADMEfromGithub.ts
@@ -1,3 +1,109 @@
-export default async function fetchREADMEfromGithub(user:string,project:string):Promise<string>{
-    return  await (await fetch(`https://raw.githubusercontent.com/${user}/${project}/main/README.md`)).text()
+import type { Root } from 'mdast';
+import { remark } from 'remark';
+import parseFrontmatter from 'remark-frontmatter';
+import emoji from 'remark-gemoji';
+import gfm from 'remark-gfm';
+import { remove } from 'unist-util-remove';
+import { visit } from 'unist-util-visit';
+
+const token: string | undefined = import.meta.env.PUBLIC_GITHUB_TOKEN;
+
+if (!token) {
+  console.error(
+    'Cannot find environment variable "PUBLIC_GITHUB_TOKEN" used for escaping GitHub API rate-limiting.'
+  );
+}
+
+/**
+ * Fetch the README for a GitHub repo based on its URL.
+ *
+ * Fetches using the GitHub API and applies some custom remark transforms
+ * to better handle content written for GitHub:
+ * - Deletes any frontmatter
+ * - Applies “GitHub-flavoured Markdown” (includes stuff like strikethrough with `~~`)
+ * - Enables “Gemoji” (`:rocket:` => 🚀)
+ * - Prepends relative links & image sources with the correct absolute base
+ */
+export default async function fetchREADMEfromGithub(
+  repoURL: string
+): Promise<string> {
+  // Remove trailing slash
+  if (repoURL.at(-1) === '/') repoURL = repoURL.slice(0, -1);
+
+  const [user, repo] = repoURL.split('/').slice(-2);
+  const url = `https://api.github.com/repos/${user}/${repo}/readme`;
+
+  const response = await fetch(url, {
+    headers: {
+      accept: 'application/vnd.github.v3.json',
+      Authorization:
+        token && `Basic ${Buffer.from(token, 'binary').toString('base64')}`,
+      'User-Agent': 'Astro-Hackathon-Showcase',
+    },
+  });
+
+  if (response.status >= 400) {
+    console.error(response);
+    return 'No README found!';
+  }
+
+  const { content, download_url, html_url } = await response.json();
+
+  const processor = remark()
+    .use(parseFrontmatter)
+    .use(stripFrontmatter)
+    .use(gfm)
+    .use(emoji)
+    .use(absoluteImageURLs, { base: stripFilename(download_url) })
+    .use(absoluteLinks, { base: stripFilename(html_url) });
+
+  // Parse the Markdown returned by the API which is encoded in base-64
+  const md = Buffer.from(content, 'base64').toString('utf-8');
+
+  // Run the Markdown through the remark processing pipeline
+  const processedMd = (await processor.process(md)).toString();
+
+  return processedMd;
+}
+
+/** Remove the last segment of a URL, kind of like `dirname`. */
+function stripFilename(url: string) {
+  return url.split('/').slice(0, -1).join('/') + '/';
+}
+
+/** Remark plugin to prepend an absolute path in front of image src URLs. */
+function absoluteImageURLs({ base }: { base: string }) {
+  function visitor(node) {
+    // Sanitize URL by removing leading `/`
+    const relativeUrl = node.url.replace(/^.?\//, '');
+    node.url = new URL(relativeUrl, base).href;
+  }
+
+  function transform(tree: Root) {
+    visit(tree, 'image', visitor);
+  }
+
+  return transform;
+}
+
+/** Remark plugin to prepend an absolute path in front of link hrefs. */
+function absoluteLinks({ base }: { base: string }) {
+  function visitor(node) {
+    // Sanitize URL by removing leading `/`
+    const relativeUrl = node.url.replace(/^.?\//, '');
+    node.url = new URL(relativeUrl, base).href;
+  }
+
+  function transform(tree: Root) {
+    visit(tree, 'link', visitor);
+  }
+
+  return transform;
+}
+
+/** Remark plugin to strip frontmatter parsed with `remark-frontmatter`. */
+function stripFrontmatter() {
+  return function transform(tree: Root) {
+    remove(tree, 'yaml');
+  };
 }
