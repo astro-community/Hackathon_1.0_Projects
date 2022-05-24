6ce25ab629ef8f139a4720a061c0e0b9d68ea5ca
delucis <swithinbank@gmail.com>
2022-05-25 00:26:00 +0200

    OpenGraph/SEO tags

--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://hackathon-1-0-projects.vercel.app/',
   markdown: {
     // Can be 'shiki' (default), 'prism' or false to disable highlighting
     syntaxHighlight: 'prism',
