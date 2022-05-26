04b094d9cc103e215011ef62cae310049ffbe703
delucis <swithinbank@gmail.com>
2022-05-26 10:16:27 +0200

    Display screenshot of live preview on project pages

--- www/tailwind.config.cjs
+++ www/tailwind.config.cjs
@@ -1,7 +1,11 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			aspectRatio: {
+				og: '1200 / 630'
+			}
+		},
 	},
 	plugins: [require("@tailwindcss/typography"), require("daisyui")],
 }
